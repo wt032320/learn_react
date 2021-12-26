eaee6336df16d78af7035b9291232774b1f87dfe
wt032320 <1476081727@qq.com>
2021-12-26 17:05:32 +0800

    feat: reactThought

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -94,7 +94,17 @@ const sculptureList = [{
   alt: 'A group of bronze hippo sculptures emerging from the sett sidewalk as if they were swimming.'
 }];
 
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
 export {
   people,
-  sculptureList
+  sculptureList,
+  PRODUCTS
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import ReactDom from 'react-dom'
 
-import List from './five'
-//import Form from './four'
-import Accordion from './seven.'
-import Form1 from './six'
+import TableComponents from './reactThought'
+
+// import List from './five'
+// import Form from './four'
+// import Accordion from './seven.'
+// import Form1 from './six'
 
 // import Gallery1 from './one'
 // import Gallery3 from './three'
@@ -13,12 +15,7 @@ import Form1 from './six'
 function App() {
   return (
     <>
-      {/* <Gallery1 />
-      <Gallery2 />
-      <Gallery3 /> */}
-      <Form1 />
-      <List />
-      <Accordion />
+      <TableComponents />
     </>
   );
 }
