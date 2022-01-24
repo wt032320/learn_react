860fc9c0540443e106e044b32cb860e637f69157
wt032320 <1476081727@qq.com>
2022-01-24 19:03:43 +0800

    feat: State

--- interactivity/src/App.jsx
+++ interactivity/src/App.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 // import Button from './Responding/Button'
 // import ToolBar from './Responding/ToolBar'
 // import ToolSide from './Responding/ToolSide'
-import LightSwitch from './Responding/LightSwitch'
+// import LightSwitch from './Responding/LightSwitch'
+import Gallery from './State/Gallery'
 
 function App() {
   return (
-    <LightSwitch />
+    <Gallery />
   )
 }
 
---
+++ interactivity/src/State/Gallery.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+import { sculptureList } from '../data/data'
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
+
+  function handleNextClick() {
+    setIndex(index + 1)
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore)
+  }
+
+  const sculpture = sculptureList[index]
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleNextClick}
+      >
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name}</i>
+        by
+        {sculpture.artist}
+      </h2>
+      <h3>
+        (
+        {index + 1}
+        of
+        {sculpture.length}
+        )
+      </h3>
+      <button
+        type="button"
+        onClick={handleMoreClick}
+      >
+        {showMore ? 'Hide' : 'Show'}
+        details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+    </>
+  )
+}
